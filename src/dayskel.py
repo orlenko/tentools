7c45d3f91ac5344f142a8470d25e49527767dde2
Vlad Orlenko <vlad@noom.com>
2020-08-10 16:49:56 -0400

    Day skeleton generator tool

---
+++ src/dayskel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import sys
+from datetime import datetime, timedelta
+from appscript import *
+
+
+EMAIL = os.environ['ICAL_EMAIL']
+
+def get_events(email):
+    the_cal = None
+    for c in app('iCal').calendars():
+        if c.name() == email:
+            the_cal = c
+            break
+    if not the_cal:
+        return
+    now = datetime.now()
+    start = datetime(now.year, now.month, now.day)
+    end = start + timedelta(1)
+    return the_cal.events[(its.start_date >= start).AND(its.start_date < end)]()
+
+
+def mktemplate(events):
+    now = datetime.now()
+    parts = ['Day Notes for %s' % now.strftime('%b %d %Y')]
+    for evt in sorted(events, key=lambda evt: evt.start_date()):
+        start = evt.start_date()
+        end = evt.end_date()
+        parts.append('[%02d:%02d—%02d:%02d] %s' % (start.hour, start.minute, end.hour, end.minute, evt.summary()))
+    return '\n'.join(parts)
+
+
+if __name__ == '__main__':
+    evts = get_events(EMAIL)
+    print(mktemplate(evts))
